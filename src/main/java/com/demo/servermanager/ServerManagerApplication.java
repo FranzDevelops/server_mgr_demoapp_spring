f8195fea9ebacd8692921517ecf9974d343b48c3
FranzDevelops <franz.developer@proton.me>
2023-06-01 19:06:17 -0600

    HTTP Resource implementation

--- src/main/java/com/demo/servermanager/ServerManagerApplication.java
+++ src/main/java/com/demo/servermanager/ServerManagerApplication.java
@@ -1,7 +1,12 @@
 package com.demo.servermanager;
 
+import com.demo.servermanager.enumeration.Status;
+import com.demo.servermanager.model.Server;
+import com.demo.servermanager.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerManagerApplication {
@@ -10,4 +15,11 @@ public class ServerManagerApplication {
 		SpringApplication.run(ServerManagerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC",
+					"http://localhost/server/image/server1.png", Status.SERVER_UP));
+		};
+	}
 }
